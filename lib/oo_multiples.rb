e27907523e71cce39cf914b7d9fd9d6f41780b4f
Josh Pearson <jpearson846@gmail.com>
2018-07-19 14:52:33 -0400

    Update oo_multiples.rb

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples()
+      multiples = []
+      for x in 1..(@limit - 1) do
+        if (x % 5 === 0) || (x % 3 === 0)
+          multiples << x
+        end
+      end
+    multiples;
+  end
+
+  def sum_multiples()
+      sum = 0
+      for x in 1..(@limit - 1) do
+        if (x % 5 === 0) || (x % 3 === 0)
+          sum += x
+        end
+      end
+    sum;
+  end
+
+
+end
