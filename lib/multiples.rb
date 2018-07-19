03b3bd46964dc040aa0ef79d4f35ae7edf029430
Josh Pearson <jpearson846@gmail.com>
2018-07-19 14:52:14 -0400

    Update multiples.rb

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    multiples = []
+    for x in 1..(limit - 1) do
+      if (x % 5 === 0) || (x % 3 === 0)
+        multiples << x
+      end
+    end
+  multiples;
+end
+
+def sum_multiples(limit)
+    sum = 0
+    for x in 1..(limit - 1) do
+      if (x % 5 === 0) || (x % 3 === 0)
+        sum += x
+      end
+    end
+  sum;
+end
